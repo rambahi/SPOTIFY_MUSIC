c426112751049e547a59f40abbf74a3b1db6d837
rambahi <pk0031641@gmail.com>
2025-07-13 20:52:05 +0530

    Update config.py
    Signed-off-by: rambahi <pk0031641@gmail.com>

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/SPOTIFY_MUSIC",
+    "https://github.com/rambahi/SPOTIFY_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
